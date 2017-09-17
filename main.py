1755daaec474ae38662c12a53ad5949008bdcaff
Axel Steingrimsson <axelthorstein@gmail.com>
2017-09-17 00:16:59 -0400

    v1

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import logging
-import requests
-import mimetypes
 
-from flask import Flask, render_template, url_for
-import pyrebase
+from flask import Flask, render_template
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db as database
 from firebase_admin import storage
-from firebase_admin import auth
 from oauth2client.service_account import ServiceAccountCredentials
 
 from watson_developer_cloud import VisualRecognitionV3
@@ -17,31 +13,19 @@ from gifextract import get_frame
 
 app = Flask(__name__)
 
+
 global admin
-admin = firebase_admin.initialize_app(credentials.Certificate('firebase_credentials.json'), name='panel-180102', options={"databaseURL": "https://panel-180102.firebaseio.com/"})
+admin = firebase_admin.initialize_app(
+    credentials.Certificate('firebase_credentials.json'),
+    name='panel-180102',
+    options={"databaseURL": "https://panel-180102.firebaseio.com/"})
 
 
 def detect_image(file_path):
-    import json
-    watson = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/detect_faces?api_key=9b4cc1ac0342de1c531f65f8c2d4410c42b7357f&version=2016-05-20"
     visual_recognition = VisualRecognitionV3('2016-05-20', api_key='9b4cc1ac0342de1c531f65f8c2d4410c42b7357f')
     image_data = visual_recognition.detect_faces(images_url=file_path)
-    data = []
-    for face in image_data['images'][0]['faces']:
-        data.append(face)
 
-    return data
-
-def initialize_firebase():
-    cred = credentials.Certificate('firebase_credentials.json')
-    admin = firebase_admin.initialize_app(cred, name='panel-180102', options={"databaseURL": "https://panel-180102.firebaseio.com/"})
-
-    return admin
-
-def get_admin():
-    if not firebase_admin:
-        return initialize_firebase()
-    return firebase_admin.get_app()
+    return [face for face in image_data['images'][0]['faces']]
 
 
 @app.route('/grid')
@@ -49,49 +33,67 @@ def grid():
     return render_template('grid.html')
 
 
-@app.route('/images/<id>', methods=["GET"])
-def images(id):
-    bucket = storage.bucket(name="panel-180102.appspot.com", app=admin)
+def get_image_url(id):
+    image_url = "https://firebasestorage.googleapis.com/v0/b/panel-180102.appspot.com/o/images%2f" \
+        + id \
+        + ".png?alt=media&token="
+    scopes = ['https://www.googleapis.com/auth/firebase.database',
+              'https://www.googleapis.com/auth/userinfo.email',
+              "https://www.googleapis.com/auth/cloud-platform"]
 
-    gender_agg = database.reference("data/gender_agg", app=admin).get()
-
-    age_agg = database.reference("data/age_agg", app=admin).get()
-
-    filename = id + ".gif"
-
-    blob = bucket.blob("gifs/" + filename)
-    blob.download_to_filename("assets/" + filename)
-
-    get_frame("assets/" + filename, id, database, bucket)
-
-    #image_url = bucket.blob("images/" + id + ".png").generate_signed_url(100, method="GET", )
-    image_url = "https://firebasestorage.googleapis.com/v0/b/panel-180102.appspot.com/o/images%2f" + id + ".png?alt=media&token="
-    scopes = [
-                        'https://www.googleapis.com/auth/firebase.database',
-                        'https://www.googleapis.com/auth/userinfo.email',
-                        "https://www.googleapis.com/auth/cloud-platform"
-                    ]
     credentials = ServiceAccountCredentials.from_json_keyfile_name('firebase_credentials.json', scopes)
 
-    image_data = detect_image(image_url + credentials.get_access_token().access_token)
+    return detect_image(image_url + credentials.get_access_token().access_token)
+
+
+def update_gender_aggregate(image_data, id, face):
+    gender_agg = database.reference("data/gender_agg", app=admin).get()
+    database.reference("images/" + id, app=admin).set(image_data)
+
+    if face['gender']['score'] > 0.15:
+        if not gender_agg:
+            gender_agg = {"MALE": 0, "FEMALE": 0}
+        if face['gender']['gender'] == "MALE":
+            gender_agg["MALE"] += 1
+        else:
+            gender_agg["FEMALE"] += 1
+        database.reference("data/gender_agg", app=admin).update(gender_agg)
+
+
+def update_age_aggregate(image_data, id, face):
+    age_agg = database.reference("data/age_agg", app=admin).get()
+    if not age_agg:
+        age_agg = {"avg_age": 20, "num_ages": 1}
+    age_agg['avg_age'] = ((age_agg['avg_age'] * age_agg['num_ages'])
+                          + ((face["age"]["max"]
+                              + face["age"]["min"]) / 2)) / (age_agg['num_ages'] + 1)
+    age_agg['num_ages'] += 1
+
+    database.reference("data/age_agg", app=admin).update(age_agg)
+
+
+def update_aggregates(image_data, id):
     for face in image_data:
-        database.reference("images/" + id, app=admin).set(image_data)
-        if face['gender']['score'] > 0.26:
-            if not gender_agg:
-                gender_agg = {"MALE": 0, "FEMALE": 0}
-            if face['gender']['gender'] == "MALE":
-                gender_agg["MALE"] += 1
-            else:
-                print(image_data)
-                gender_agg["FEMALE"] += 1
-            database.reference("data/gender_agg", app=admin).update(gender_agg)
+        update_gender_aggregate(image_data, id, face)
+        update_age_aggregate(image_data, id, face)
 
-        if not age_agg:
-            age_agg = {"avg_age": 20, "num_ages": 1}
-        age_agg['avg_age'] = ((age_agg['avg_age'] * age_agg['num_ages']) + ((face["age"]["max"] + face["age"]["min"]) / 2)) / (age_agg['num_ages'] + 1)
-        age_agg['num_ages'] += 1
 
-        database.reference("data/age_agg", app=admin).update(age_agg)
+@app.route('/images/<id>', methods=["GET"])
+def images(id):
+    filename = id + ".gif"
+    bucket = storage.bucket(name="panel-180102.appspot.com", app=admin)
+
+    # Download the gif locally
+    bucket.blob("gifs/" + filename).download_to_filename("assets/" + filename)
+
+    # Extract the middle frame from the gif
+    get_frame("assets/" + filename, id, database, bucket)
+
+    # Extract age and gender from the frame with IBM's Watson Image Recognition API
+    image_data = get_image_url(id)
+
+    # Update the gender and age aggregates
+    update_aggregates(image_data, id)
 
     return id
 
@@ -106,6 +108,6 @@ def server_error(e):
 
 
 if __name__ == '__main__':
-# This is used when running locally. Gunicorn is used to run the
-# application on Google App Engine. See entrypoint in app.yaml.
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
